7df4ccf85e55e4a8feccde07922b2a43db11174a
Ashaffah <wagamama.morph@gmail.com>
2024-03-15 19:04:25 +0700

    fix(get_svg_post): qr code can't be scanned

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@
 extern crate rocket;
 
 use qirust::helper::generate_svg_string;
-use rocket::data::ByteUnit;
-use rocket::Data;
+// use rocket::{ data::ToByteUnit, Data };
 use rocket::serde::json::Json;
 use std::collections::HashMap;
 use rocket::http::Method;
 use rocket_cors::{ AllowedOrigins, CorsOptions };
+use rocket::serde::{ Deserialize, Serialize };
+
+// Define a struct to represent the incoming JSON
+#[derive(Serialize, Deserialize)]
+struct SvgData {
+    content: String,
+}
 
 #[get("/svg?<data>")]
 async fn get_svg(data: String) -> Json<HashMap<String, String>> {
@@ -20,18 +26,25 @@ async fn get_svg(data: String) -> Json<HashMap<String, String>> {
     Json(map)
 }
 
-#[post("/svg", data = "<content>")]
-async fn get_svg_post(content: Data<'_>) -> Json<HashMap<String, String>> {
-    let param_string: String = content
-        .open(ByteUnit::default())
-        .into_string().await
-        .unwrap()
-        .to_string();
-    let svg_string = generate_svg_string(&param_string);
+// result of qr code = "Hello World"
+#[post("/svg", format = "json", data = "<svg_data>")]
+async fn get_svg_post(svg_data: Json<SvgData>) -> Json<HashMap<String, String>> {
+    let svg_string = generate_svg_string(&svg_data.content);
     let mut map = HashMap::new();
     map.insert("svg".to_string(), svg_string);
     Json(map)
 }
+
+// result of qr code = { "content": "Hello World" }
+// #[post("/svg", data = "<data>")]
+// async fn get_svg_post(data: Data<'_>) -> Json<HashMap<String, String>> {
+//     let body = data.open((256).kibibytes()).into_bytes().await.unwrap();
+//     let data_string = String::from_utf8_lossy(&body).to_string();
+//     let svg_string = generate_svg_string(&data_string);
+//     let mut map = HashMap::new();
+//     map.insert("svg".to_string(), svg_string);
+//     Json(map)
+// }
 
 #[launch]
 fn rocket() -> _ {
